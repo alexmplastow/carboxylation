a8dc4493f3fc32be3b28d7af493bef897431d828
alexmplastow <79552389+alexmplastow@users.noreply.github.com>
2025-12-23 15:25:03 -0600

    Update productionCarboxylationPosition_2_tetrahedral.py

--- productionCarboxylationPosition_2_tetrahedral.py
+++ productionCarboxylationPosition_2_tetrahedral.py
@@ -3,6 +3,8 @@ import objects
 
 import json
 import copy
+from tqdm import tqdm
+import warnings
 
 #################################################################
 #Parameters
@@ -10,46 +12,87 @@ import copy
 
 fileID = '/home/alpal/projects/methanCapture/carboxylationProblem/db/simah.db'
 
+XYZfile = '/home/alpal/projects/methanCapture/carboxylationProblem/filtered_structures_6256.xyz'
+
+fmaxList = [0.5, 0.25, 0.1, 0.05, 0.025, 0.01]
+
+debug = True
+
+switchR1andR2 = True
+
+outputDir = 'outputs_αR2_tetrahedral'
+
+planar = False
+
 #################################################################
 #Main
 #################################################################
 
-#NOTE: These are not file paths, they are just strings
-xyzList = functions.getXYZstructureList(fileID)
-XYZobjects = [objects.xyzStructure(xyzString) for xyzString in xyzList]
+_, structureCodes = functions.getXYZandBuildingBlock_structureList(fileID, XYZfile, sanityCheck = False)
 
-#xyzObject = XYZobjects[0]
+XYZobjects = functions.load_xyz_structures_from_file(XYZfile)
 
-#fiveMemberedRingInstance = objects.fiveMemberedRing()
-	
-#xyzObjectOrigninal = copy.deepcopy(xyzObject)
-
-#xyzObject.viewInVMD()
-#xyzObject.constructRingIntermediate(fiveMemberedRingInstance, switchR1andR2 = False)
-
-
-for i, xyzObject in enumerate(XYZobjects):
+def xyzObjectCarboxylationRoutine(xyzObject, switchR1andR2 = False, planar = True, outputDir = 'outputs_αR1_planar'):
 
 	fiveMemberedRingInstance = objects.fiveMemberedRing()
-	
-	xyzObjectOrigninal = copy.deepcopy(xyzObject)
 
-	try:
-		xyzObject.constructRingIntermediate(fiveMemberedRingInstance, switchR1andR2 = True)
+	xyzObject.constructRingIntermediate(fiveMemberedRingInstance, switchR1andR2 = False)
 	
-	except:
+	if planar:
 
-		print("I haven't configured most of these routines to handle cases of ligand complexes without one of the Ni branches, I'm assuming that's what this is")
-		continue
-	try:
+		xyzObject.forcePlanar()
+	else:
 		xyzObject.forceTetrahedral()
-		xyzObject.reduceRclashes(R_index = 1)
-		xyzObject.reduceRclashes(R_index = 2)
-		xyzObject.pivotCorrectionForValenceSanity()
-		xyzObject.printToFile(f"outputs_αR2_tetrahedral/ligands_{str(i).zfill(4)}.xyz")
-		xyzObject.writeSanityRecord(f"outputs_αR2_tetrahedral/ligands_{str(i).zfill(4)}_sanityRecord.txt", hydrogenSkip = True)
 
+	xyzObject.reduceRclashes(R_index = 1, rotationAngle = 20)
+	xyzObject.reduceRclashes(R_index = 2, rotationAngle = 20)
+
+	xyzObject.pivotCorrectionForValenceSanity(rotationAngle = 20)
+
+	xyzObject.reduceRclashes(R_index = 1, rotationAngle = 20)
+	xyzObject.reduceRclashes(R_index = 2, rotationAngle = 20)
+
+	xyzObject.printToFile(f"{outputDir}/ligands_{str(i).zfill(4)}_{structureCodes[i]}.xyz")
+	xyzObject.writeSanityRecord(f"{outputDir}/ligands_{str(i).zfill(4)}_{structureCodes[i]}_sanityRecord.txt")
+
+for i, xyzObject in tqdm(enumerate(XYZobjects)):
+
+	xyzObjectCopy = copy.deepcopy(xyzObject)
+	converged = False
+	fIndex = 0
+
+	try:
+		xyzObjectCarboxylationRoutine(xyzObject, switchR1andR2, planar, outputDir)
 	except:
-		print("That's not good")
-	
-	
+
+		warnings.warn("InitialCarboxlyation failed, trying again with a DFT approximation to fix failed bonds")
+
+		while not converged:
+
+			if fIndex > len(fmaxList):
+				print(f"Optimization failed, take note of structure {i}")
+				#NOTE: it is "Converged"
+				converged = True
+				continue
+
+			#NOTE: xyzObjectCopy is not going to be redefined
+				#NOTE: ; therefore, it is the original structure from the xyz
+
+			xyzObject = copy.deepcopy(xyzObjectCopy)
+
+			try:
+
+				xyzObject.geometryOptimization(fmax = fmaxList[fIndex])
+				xyzObjectCarboxylationRoutine(xyzObject, switchR1andR2, planar, outputDir)
+				converged = True
+
+			except:
+				print(("carboxylation with optimization failed, trying again with\n"
+					"stricter convergence criterion"
+				))
+				fIndex+=1
+				#NOTE: does nothing, but makes my code more readable
+				converged = False
+
+
+
